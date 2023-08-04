8b205548e6d525d29591be759d94bddfb1b94756
Emill Daniel Garcia Castellanos <Danielemill2012@gmail.com>
2023-08-04 14:13:24 -0400

    Actualizacion de la clase.

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,15 +1,30 @@
 
 class SmartDevice {
+    val name = "Android TV"
+    val category = "Entertainment"
+    var deviceStatus = "online"
+
+    var speakerVolume = 2
+        get() = field
+        set(value) {
+            if (value in 0..100) {
+                field = value
+            }
+        }
+
+
+
     fun turnOn(){
         println("El dispositivo se ha encendido.")
     }
-
     fun turnOff(){
         println("El dispositivo se ha apagado.")
     }
+
 }
 
 fun main(args: Array<String>) {
     val smartTvDevice = SmartDevice()
-    smartTvDevice.turnOn();
+    println("Device name is: ${smartTvDevice.name}")
+    smartTvDevice.turnOn()
 }
